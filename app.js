4f7140536e8dcd0acafad73466bc83d1f91e8e9e
Blu <90506743+BlueG15@users.noreply.github.com>
2024-01-27 15:38:21 +0700

    updated app.js to test room functionalities

--- app.js
+++ app.js
@@ -15,21 +15,59 @@ async function main(){
 
   const defTestPlayerID1 = new Array(20).fill("A").join("")
   const defTestPlayerID2 = new Array(20).fill("B").join("")
+  const defTestPlayerID3 = new Array(20).fill("C").join("")
+  const defTestPlayerID4 = new Array(20).fill("D").join("")
   express.get("/createRoom", async (req, res) => {
     let a = await roomController.createRoom(defTestPlayerID1, "P1")
-    res.send(JSON.stringify(a))
+    res.send(JSON.stringify(a, null, 4))
   })
 
   express.get("/joinRoom/:roomID", async (req, res) => {
     let roomID = req.params.roomID
     let a = await roomController.addToRoom(defTestPlayerID2, "P2", roomID)
-    res.send(JSON.stringify(a))
+    res.send(JSON.stringify(a, null, 4))
   })
 
   express.get("/roomData/:roomID", async (req, res) => {
     let roomID = req.params.roomID
     let a = await roomController.getRoomData(roomID)
-    res.send(JSON.stringify(a))
+    res.send(a.p1ID + "\n\n" + JSON.stringify(a, null, 4))
+  })
+
+  express.get("/joinAsSpectator/:roomID", async (req, res) => {
+    let roomID = req.params.roomID
+    let a = await roomController.addSpectator(defTestPlayerID3, roomID)
+    res.send(JSON.stringify(a, null, 4))
+  })
+
+  express.get("/joinAsSpectator2/:roomID", async (req, res) => {
+    let roomID = req.params.roomID
+    let a = await roomController.addSpectator(defTestPlayerID4, roomID)
+    res.send(JSON.stringify(a, null, 4))
+  })
+
+  express.get("/leaveRoom1/:roomID", async (req, res) => {
+    let roomID = req.params.roomID
+    let a = await roomController.removeFromRoom(defTestPlayerID1, roomID)
+    res.send(JSON.stringify(a, null, 4))
+  })
+
+  express.get("/leaveRoom2/:roomID", async (req, res) => {
+    let roomID = req.params.roomID
+    let a = await roomController.removeFromRoom(defTestPlayerID2, roomID)
+    res.send(JSON.stringify(a, null, 4))
+  })
+
+  express.get("/leaveRoom3/:roomID", async (req, res) => {
+    let roomID = req.params.roomID
+    let a = await roomController.removeFromRoom(defTestPlayerID3, roomID)
+    res.send(JSON.stringify(a, null, 4))
+  })
+
+  express.get("/leaveRoom4/:roomID", async (req, res) => {
+    let roomID = req.params.roomID
+    let a = await roomController.removeFromRoom(defTestPlayerID4, roomID)
+    res.send(JSON.stringify(a, null, 4))
   })
 
   await roomController.bootstrap();
