4f002f4b9c0fa91c4c6c0cbe1c33c63da9ec02e5
Blu <90506743+BlueG15@users.noreply.github.com>
2024-08-08 00:55:02 +0700

    Delete events directory

--- events/ping.js
+++
@@ -1,9 +0,0 @@
-const eventController = require(`../public/eventSystem.js`)
-
-module.exports = (inputObj) => {
-    eventController.addEvent(eventController.currentTime + 5 * eventController.timePerFrame, "", "sendOveride", "pong", {}, "repeatedPong", [inputObj.playerID])
-    return {
-        'sendToCauseIgnoreCheck' : "pong",
-        'globalEventOveride' : "pong"
-    }
-}
